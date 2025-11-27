27bdf1c90e36053b6512bd7d47628c8e77d8b8ef
GitGudBro <faviocanevaro@gmail.com>
2025-11-27 13:20:12 -0500

    Revert "Mejor conexión con la bd al no dejar el puerto siempre abierto (sugerencia del chad)"
    This reverts commit 755bc173e288913a35691cffeda29159453d39ae.

--- Datos/ConexionDB.cs
+++ Datos/ConexionDB.cs
@@ -11,18 +11,18 @@ namespace Datos
     {
         //datos de la conexión
         SqlConnection conex = new SqlConnection();
-        private static string servidor = "localhost";
-        private static string bd = "Bazar";
-        private static string usuario = "BazarUser";
-        private static string contraseña = "ContraseñaUltrasegura123";
-        private static string puerto = "49678"; //un lío tremendo para encontrar y conectar el puerto
+        static string servidor = "localhost";
+        static string bd = "Bazar";
+        static string usuario = "BazarUser";
+        static string contraseña = "ContraseñaUltrasegura123";
+        static string puerto = "49678"; //un lío tremendo para encontrar y conectar el puerto
 
 
         //juntarlo todo en un string para mandarlo al objeto de tipo SQLConnection en el atributo ConnectionString
-        private static string cadenaConexion = "Data Source =" + servidor + "," + puerto + ";" + "user id=" + usuario + ";" + "password=" + contraseña + ";" + "Initial Catalog=" + bd + ";" + "Persist Security Info=true";
+        string cadenaConexion = "Data Source =" + servidor + "," + puerto + ";" + "user id=" + usuario + ";" + "password=" + contraseña + ";" + "Initial Catalog=" + bd + ";" + "Persist Security Info=true";
 
 
-        //lo hice con un tutorial de yt
+
         public SqlConnection establecerConexion()
         {
             //atrapamos los posibles errores o mandamos un mensaje de éxito
@@ -37,15 +37,6 @@ namespace Datos
                 Console.WriteLine("Error de conexión con la BD" + e.ToString());
             }
             return conex;
-        }
-
-
-        //sugerencia del chad dizque para no dejar la conexión abierta, sino estática y reutilizable y así poder abrirla en cada repositorio
-        public static SqlConnection obtenerConexion()
-        {
-            var cn = new SqlConnection(cadenaConexion);
-            cn.Open();
-            return cn;
         }
     }
 }
